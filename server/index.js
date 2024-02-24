dcb91e2e1e8aae00c47ee3db3613fa752f043aff
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-02-24 23:42:29 +0530

    backend implementation

---
+++ server/index.js
@@ -0,0 +1,106 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require("cors")
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// database config
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb")
+const uri =
+    "mongodb+srv://buybooks-mern-web-application:kV2wISUcwdis2wMy@cluster0.kgepmtd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+})
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect()
+
+        // create collection of database
+        const booksCollection = client.db("bookInventory").collection("books")
+
+        // inset a book to the database - POST method
+        app.post("/upload-book", async (req, res) => {
+            const data = req.body
+            const result = await booksCollection.insertOne(data)
+            res.send(result)
+        })
+
+        // get all books from the database - GET method
+        app.get("/all-books", async (req, res) => {
+            const book = booksCollection.find()
+            const result = await book.toArray()
+            res.send(result)
+        })
+
+        // update a book data - PATCH or UPDATE method
+        app.patch("/book/:id", async (req, res) => {
+            const id = req.params.id
+            const updateBookData = req.body
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: { ...updateBookData }
+            }
+
+            const options = { upsert: true }
+
+            // update
+            const result = await booksCollection.updateOne(
+                filter,
+                updateDoc,
+                options
+            )
+            res.send(result)
+        })
+
+        // delete a book data - DELETE method
+        app.delete("/book/:id", async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const result = await booksCollection.deleteOne(filter)
+            res.send(result)
+        })
+
+        // find book by category
+        app.get("/all-books", async (req, res) => {
+            const query = {}
+            if (req.query?.category) {
+                query = { category: req.query.category }
+            }
+
+            const result = await booksCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 })
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        )
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close()
+    }
+}
+run().catch(console.dir)
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+app.listen(port, () => {
+    console.log(`Buybooks app running on port ${port}`)
+})
+
+// kV2wISUcwdis2wMy
