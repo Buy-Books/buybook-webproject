52cbc174b7197b0967524fa2e10737797a1a4403
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-06-09 01:47:59 +0530

    update single book layout

--- client/src/components/SingleBook.jsx
+++ client/src/components/SingleBook.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { useLoaderData, Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import RecentBooks from "./RecentBooks"
+import { FaArrowLeft } from "react-icons/fa6"
 
 const SingleBook = () => {
     const {
@@ -14,29 +16,148 @@ const SingleBook = () => {
         price
     } = useLoaderData()
 
+    const navigate = useNavigate()
+
     return (
         <div>
-            <div className="container flex gap-6 items-center justify-center mt-6 md:gap-16 md:min-h-[80vh]">
-                <div className="flex flex-col items-center gap-6 md:flex-row md:items-start md:gap-12">
-                    <img
-                        src={imageURL}
-                        alt=""
-                        className="w-[60%] md:max-w-[400px]"
-                    />
-                    <div className="flex flex-col items-center gap-2 md:items-start">
-                        <h2 className="text-xl text-center text-dark-purple font-medium sm:text-left md:text-2xl">
-                            {bookTitle}
-                        </h2>
-                        <p>by {authorName}</p>
-                        <p>{bookDescription}</p>
-                        <p>category: {category}</p>
-                        <p>Rs.{price}.00</p>
-                        <Link to={bookPDF} target="/blank" className="btn">
-                            Buy now
-                        </Link>
+            <section className="text-gray-700 body-font overflow-hidden bg-white">
+                <div className="relative container px-5 py-24 mx-auto">
+                    <div
+                        onClick={() => navigate(-1)}
+                        className="absolute top-0 left-[20px] flex items-center justify-center size-12 bg-purple-100  p-3 rounded-full  cursor-pointer group"
+                    >
+                        <FaArrowLeft className=" text-xl text-dark-purple group-hover:text-light-purple " />
+                    </div>
+                    <div className="lg:w-4/5 mx-auto flex flex-wrap">
+                        <img
+                            alt="ecommerce"
+                            className="max-w-[300px] w-full object-contain object-center rounded border border-gray-200"
+                            src={imageURL}
+                        />
+                        <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                            <h1 className="text-2xl text-dark-purple font-medium mb-4 lg:text-3xl ">
+                                {bookTitle}
+                            </h1>
+                            <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                                {authorName}
+                            </h2>
+                            <div className="flex mb-4">
+                                <span className="flex items-center">
+                                    <svg
+                                        fill="currentColor"
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        className="w-4 h-4 text-light-purple"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                                    </svg>
+                                    <svg
+                                        fill="currentColor"
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        className="w-4 h-4 text-light-purple"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                                    </svg>
+                                    <svg
+                                        fill="currentColor"
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        className="w-4 h-4 text-light-purple"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                                    </svg>
+                                    <svg
+                                        fill="currentColor"
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        className="w-4 h-4 text-light-purple"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                                    </svg>
+                                    <svg
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        className="w-4 h-4 text-light-purple"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                                    </svg>
+                                    <span className="text-gray-600 ml-3">
+                                        4 Reviews
+                                    </span>
+                                </span>
+                                <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
+                                    <a className="text-gray-500">
+                                        <svg
+                                            fill="currentColor"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            className="w-5 h-5"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                        </svg>
+                                    </a>
+                                    <a className="ml-2 text-gray-500">
+                                        <svg
+                                            fill="currentColor"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            className="w-5 h-5"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                        </svg>
+                                    </a>
+                                    <a className="ml-2 text-gray-500">
+                                        <svg
+                                            fill="currentColor"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            className="w-5 h-5"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                        </svg>
+                                    </a>
+                                </span>
+                            </div>
+                            <p className="leading-relaxed">{bookDescription}</p>
+
+                            <div className="flex flex-col  gap-4 mt-12">
+                                <span className="title-font font-medium text-2xl text-gray-900">
+                                    Rs.{price}
+                                </span>
+                                <Link
+                                    to={bookPDF}
+                                    className="btn max-w-[200px] text-center"
+                                >
+                                    Buy now
+                                </Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
 
             <RecentBooks />
         </div>
